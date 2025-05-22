04e18e16003e81ca525703f79e4cbf48488fdfea
Gus Workman <gworkman@users.noreply.github.com>
2025-05-23 00:06:32 +0200

    initial commit

---
+++ post-build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+# Create the fwup ops script to handling MicroSD/eMMC operations at runtime
+# NOTE: revert.fw is the previous, more limited version of this. ops.fw is
+#       backwards compatible.
+mkdir -p $TARGET_DIR/usr/share/fwup
+$HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-ops.conf -o $TARGET_DIR/usr/share/fwup/ops.fw
+ln -sf ops.fw $TARGET_DIR/usr/share/fwup/revert.fw
+
+# Copy the fwup includes to the images dir
+cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
+
+# Copy bluetoothd config file
+cp -rf $BUILD_DIR/bluez5_utils-5.76/src/bluetooth.conf $TARGET_DIR/etc/dbus-1/system.d/

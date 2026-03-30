9b35c80bca0ea38e33925a96a6fb2944eec014fd
Chris Dutton <chris@aquatic-edge.com>
2026-03-30 15:27:58 +0200

    Add wlan0 activity LED on PD17
    Define the LED in the device tree with the netdev trigger, enable
    CONFIG_LEDS_TRIGGER_NETDEV in the kernel, and configure it to blink
    on wlan0 TX/RX traffic in pre-run.sh.

--- rootfs_overlay/usr/bin/pre-run.sh
+++ rootfs_overlay/usr/bin/pre-run.sh
@@ -6,3 +6,8 @@
 # Load the ESP32 SDIO WiFi driver
 # Options (resetpin, etc.) are picked up from /etc/modprobe.d/esp-hosted.conf
 /sbin/modprobe esp32_sdio
+
+# Configure wlan0 activity LED (PD17)
+echo wlan0 > /sys/class/leds/:wlan/device_name
+echo 1 > /sys/class/leds/:wlan/tx
+echo 1 > /sys/class/leds/:wlan/rx
